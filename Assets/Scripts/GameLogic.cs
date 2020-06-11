4288c4828757c912c9d3b6ae52bf54820a757d05
Tomas Stepanavicius <ttomass@gmail.com>
2020-06-11 22:31:10 +1200

    Added scripts
    Finished MainMenu, started GameLogic scripts

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour
+{
+
+    public string input;
+    public GameObject inputText;
+    public GameObject guesses;
+
+    public void AssignInput()
+    {
+        input = inputText.GetComponent<Text>().text;
+        guesses.GetComponent<Text>().text = "You entered: " + input;
+    }
+}
